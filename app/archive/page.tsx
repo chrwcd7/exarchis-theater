66aaba1611faa247ffca4bbedad1029c65ce0dfb
Chris <25690677+chrwcd7@users.noreply.github.com>
2025-01-31 14:56:10 +0200

    Update archive page

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,35 +1,92 @@
 'use client';
 
-import React from 'react';
-
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import { YearProvider, useYear } from '../context/YearContext';
 import { plays } from '../archive/plays';
+import ImageGallery from '../components/ImageGallery';
 
-const ArchiveContent = () => {
+const ArchiveContent = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
   const { selectedYear } = useYear();
 
+  // Filter plays for the selected year
+  const filteredPlays = plays.filter((play) => play.year === selectedYear);
+
   return (
-    <div className="flex-grow px-8">
+    <div className="px-8 text-center lg:text-left">
       <h1 className="text-3xl font-bold mb-4">Αρχείο</h1>
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Έτος {selectedYear}</h2>
-        <p>Displaying archive content for the year {selectedYear}.</p>
-        {/* Add more content related to the selected year here */}
+      <div className="flex flex-col items-center lg:items-start">
+        <h2 className="text-2xl font-semibold mb-4 " onClick={toggleSidebar}>
+          Έτος <span className="text-teal-500 lg:text-black">{selectedYear}</span>
+        </h2>
+        <div className="flex flex-col gap-8">
+          {filteredPlays.map((play, index) => (
+            <div key={index} className="bg-white shadow-md rounded-lg px-4 pb-4 pt-8 flex flex-col break-words relative">
+              {play.tag && (
+                <span className="bg-blue-500 text-white text-xs font-semibold px-2 py-1 rounded absolute top-2 right-2">
+                  {play.tag}
+                </span>
+              )}
+              <h3 className="text-xl font-bold">{play.title}</h3>
+              <p className="text-gray-700 mb-4">{play.screenwriter}</p>
+              {/* // TODO: Pass images to ImageGallery component as prop */}
+              {/* <ImageGallery images={play.images} /> */}
+              <div className="flex flex-col items-center lg:items-start">
+                <ImageGallery />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 const Archive = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   // Find the most recent year from the plays object
   const mostRecentYear = Math.max(...plays.map((play) => play.year));
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
 
   return (
     <YearProvider initialYear={mostRecentYear}>
       <div className="flex min-h-screen">
-        <Sidebar />
-        <ArchiveContent />
+        <button
+          className="absolute text-white bg-teal-500 p-2 rounded m-4 lg:hidden"
+          onClick={toggleSidebar}
+          aria-label="Toggle sidebar"
+        >
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 4a1 1 0 011-1h16a1 1 0 011 1v16a1 1 0 01-1 1H4a1 1 0 01-1-1V4zm1 0v16h16V4H4zm4 2h8v2H8V6zm0 4h8v2H8v-2zm0 4h8v2H8v-2z"
+            ></path>
+          </svg>
+        </button>
+        <div
+          className={`fixed inset-0 z-50 bg-black bg-opacity-50 lg:hidden ${isSidebarOpen ? 'block' : 'hidden'}`}
+          onClick={closeSidebar}
+        ></div>
+        <div
+          className={`fixed inset-y-0 left-0 z-50 w-64 bg-white shadow-lg transform ${
+            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+          } transition-transform lg:relative lg:translate-x-0`}
+        >
+          <Sidebar closeSidebar={closeSidebar} />
+        </div>
+        <div className="flex-grow">
+          <ArchiveContent toggleSidebar={toggleSidebar} />
+        </div>
       </div>
     </YearProvider>
   );
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,26 +1,37 @@
 'use client';
 
 import React from 'react';
-
 import { useYear } from '../context/YearContext';
 import { plays } from '../archive/plays';
 
-const Sidebar = () => {
+const Sidebar = ({ closeSidebar }: { closeSidebar: () => void }) => {
   const { selectedYear, setSelectedYear } = useYear();
 
   // Extract years from plays and create a sorted set of unique years
   const years = Array.from(new Set(plays.map((play) => play.year))).sort((a, b) => b - a);
 
+  const handleYearClick = (year: number) => {
+    setSelectedYear(year);
+    closeSidebar();
+  };
+
   return (
-    <div className="w-32 px-0 sm:w-64 sm:px-4">
+    <div className="relative px-0 pt-4 lg:w-64 lg:px-4">
+      <button
+        className="absolute top-2 right-2 text-4xl text-black lg:hidden"
+        onClick={closeSidebar}
+        aria-label="Close sidebar"
+      >
+        &times;
+      </button>
       <h2 className="text-xl font-bold mb-4 text-center">Έτος</h2>
       <ul className="flex flex-col items-center">
         {years.map((year) => (
           <li key={year} className="mb-2">
             <button
-              onClick={() => setSelectedYear(year)}
-              className={`text-blue-500 hover:underline ${
-                selectedYear === year ? 'bg-blue-500 !text-white font-bold' : ''
+              onClick={() => handleYearClick(year)}
+              className={`text-teal-500 hover:underline ${
+                selectedYear === year ? 'bg-teal-500 !text-white font-bold' : ''
               } px-4 py-2 rounded`}
             >
               {year}
