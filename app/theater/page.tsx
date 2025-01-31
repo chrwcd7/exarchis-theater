433ddda4dbabf705c0f6162550a880c668387c43
Chris <25690677+chrwcd7@users.noreply.github.com>
2025-01-31 14:56:23 +0200

    Update theater page

--- app/theater/page.tsx
+++ app/theater/page.tsx
@@ -1,12 +1,11 @@
 import ImageGallery from '../components/ImageGallery';
 
 export default function Theater() {
-  const images = ['/exarchis1.jpg', '/exarchis2.jpg', '/exarchis3.jpg', '/exarchis4.jpg'];
+  const images = ['/exarchis1.jpg', '/exarchis2.jpg', '/exarchis3.jpg', '/exarchis4.jpeg'];
 
   return (
     <>
       <section className="text-center mb-8">
-        <h1 className="text-4xl font-bold mb-4">Θέατρο Εξαρχής</h1>
         <p className="text-lg">
           Καλώς ήρθατε στην επίσημη σελίδα του Θεάτρου Εξαρχής! Βρισκόμενο στην καρδιά της Αθήνας, το Θέατρο Εξαρχής είναι ένας
           ζωντανός και δυναμικός χώρος που αφιερώνεται στην παρουσίαση των καλύτερων σύγχρονων και κλασικών παραστάσεων στο
