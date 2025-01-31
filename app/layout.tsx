30692791c284dd8e09ee4434a0446a7057ba7140
Chris <25690677+chrwcd7@users.noreply.github.com>
2025-01-31 14:56:12 +0200

    Add contact info to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,28 @@ export default function RootLayout({
         <main className="container mx-auto flex-grow py-8 px-4 bg-gray-100 sm:px-16">{children}</main>
         <footer className="bg-gray-800 text-white py-4">
           <div className="container mx-auto text-center">
+            <div className="mb-4">
+              <p>
+                <a
+                  href={`https://www.google.com/maps/place/%CE%95%CE%BE+%CE%91%CF%81%CF%87%CE%AE%CF%82/@37.9894193,23.7327292,17z/data=!3m1!4b1!4m6!3m5!1s0x14a1bd172dbfe379:0x8f656a2707ead660!8m2!3d37.9894151!4d23.7353041!16s%2Fg%2F11g__w64kl?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D`}
+                  className="text-teal-500 hover:underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Σπύρου Τρικούπη 44, Εξάρχεια, Αθήνα
+                </a>
+              </p>
+              <p>
+                <a href="tel:2103822661" className="text-teal-500 hover:underline">
+                  210 382 2661
+                </a>
+              </p>
+              <p>
+                <a href="mailto:exarhistheatro@gmail.com" className="text-teal-500 hover:underline">
+                  exarhistheatro@gmail.com
+                </a>
+              </p>
+            </div>
             <p>&copy; 2025 Θέατρο Εξαρχής. All rights reserved.</p>
           </div>
         </footer>
