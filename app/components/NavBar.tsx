87e78493b3d0cc4e501a0580613f701a05b81e07
Chris <25690677+chrwcd7@users.noreply.github.com>
2025-01-31 14:55:42 +0200

    Update NavBar with logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import {
   NavigationMenu,
@@ -10,37 +11,41 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import logo from '../../public/exarchis_logo.jpg';
 
 export default function NavBar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/about-us" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Ποιοι είμαστε</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/theater" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Θέατρο</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/news" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Νέα</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/contact-us" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Επικοινωνία</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/archive" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Αρχείο</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className="bg-black w-full flex flex-col justify-center items-center py-4 sm:flex-row">
+      <Image src={logo} alt="logo" width={100} height={100} className="mb-6 sm:mr-12 sm:mb-0" />
+      <NavigationMenu>
+        <NavigationMenuList className="flex flex-col items-start sm:flex-row">
+          <NavigationMenuItem>
+            <Link href="/about-us" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Ποιοι ειμαστε</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/theater" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Θεατρο</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/news" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Νεα</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/contact-us" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Επικοινωνια</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/archive" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>Αρχειο</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
   );
 }
