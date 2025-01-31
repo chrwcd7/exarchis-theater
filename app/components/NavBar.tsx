c0e4a81b533fb9d58f7d20b98fc77a27817f98f1
Chris <25690677+chrwcd7@users.noreply.github.com>
2025-01-31 14:55:46 +0200

    Add button for toggling the navigation menu on mobile

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 import {
   NavigationMenu,
@@ -16,14 +17,27 @@ import logo from '../../public/exarchis_logo.jpg';
 
 export default function NavBar() {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const isActive = (route: string) => pathname === route;
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="bg-black w-full flex flex-col justify-center items-center py-4 sm:flex-row">
-      <Link href={'/'}>
-        <Image src={logo} alt="logo" width={100} height={100} className="mb-6 sm:mb-0" />
-      </Link>
-      <NavigationMenu>
+    <div className={`bg-black w-full flex flex-col justify-center items-center py-4 sm:flex-row`}>
+      <div className="flex flex-row gap-4">
+        <Link href={'/'}>
+          <Image src={logo} alt="logo" width={100} height={100} className="mb-6 sm:mb-0" />
+        </Link>
+        <button className="sm:hidden text-white" onClick={toggleMenu} aria-label="Toggle navigation menu">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+          </svg>
+        </button>
+      </div>
+      <NavigationMenu className={`${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
         <NavigationMenuList className="flex flex-col items-start sm:flex-row sm:ml-12">
           <NavigationMenuItem>
             <Link href="/about-us" legacyBehavior passHref>
