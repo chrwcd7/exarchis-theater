556d410032cd55d12bf4c2415c45c89b1f22575f
Chris <chrwcd7@users.noreply.github.com>
2025-03-08 14:32:53 +0200

    Make article container flex

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -9,22 +9,20 @@ const News = () => {
   return (
     <>
       <h1 className="text-3xl font-bold mb-6">Νέα</h1>
-      {articles.map((article) => (
-        <article
-          key={article.id}
-          className="mb-8 bg-card p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 last:mb-0"
-        >
-          <Link href={`/news/${article.id}`} className="!no-underline dark:text-teal-500">
-            <h2 className="text-2xl mb-2">{article.title}</h2>
-            <p className="text-secondary-foreground mb-4 font-normal italic">{article.date}</p>
-            <div
-              className="text-lg leading-relaxed text-foreground font-normal line-clamp-3"
-              dangerouslySetInnerHTML={{ __html: article.content }}
-            >
-            </div>
-          </Link>
-        </article>
-      ))}
+      <div className="flex flex-col-reverse gap-8">
+        {articles.map((article) => (
+          <article key={article.id} className="bg-card p-6 rounded-lg shadow-md transition-transform transform hover:scale-105">
+            <Link href={`/news/${article.id}`} className="!no-underline dark:text-teal-500">
+              <h2 className="text-2xl mb-2">{article.title}</h2>
+              <p className="text-secondary-foreground mb-4 font-normal italic">{article.date}</p>
+              <div
+                className="text-lg leading-relaxed text-foreground font-normal line-clamp-3"
+                dangerouslySetInnerHTML={{ __html: article.content }}
+              ></div>
+            </Link>
+          </article>
+        ))}
+      </div>
     </>
   );
 };
